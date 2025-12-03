928a711143c2aa7fa160c7343d03721121feb688
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2025-12-03 13:03:01 +0200

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import TanStackProvider from "@/components/providers/TanStackProvider";
 import AuthProvider from "@/components/providers/AuthProvider";
+
+import "@/styles/global.css";
+import "@/styles/reset.css";
+import "@/styles/variables.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
